31dfeac9bbc1efd4bfe1b0dba4e079f3a42b0d17
Aimee Knight <AimeeKnight83@gmail.com>
2013-09-30 13:05:08 -0400

    begin basic Sinatra configuration

---
+++ main.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'json'
+require 'net/http'
+require 'yaml'
+require 'sinatra'
+require 'sinatra/flash'
+
+zip = "31324"
+
+get '/' do    
+    erb :index  
+end  
+
+post '/' do
+	zip 			= params[:zip].to_s
+	CONFIG 		= YAML.load_file("config.yml")
+	key 	 		= CONFIG['petsearch']['key']
+	url		 		= "http://api.petfinder.com/pet.find?key=#{key}&format=json&location=#{zip}"
+	resp   		= Net::HTTP.get_response(URI.parse(url))
+	resp_text = resp.body
+	obj 			= JSON.parse(resp_text)
+	@pets     = obj["petfinder"]["pets"]["pet"]
+	erb :index
+end
