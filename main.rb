bce163621c411dc51f356a396f668be724062695
Aimee Knight <AimeeKnight83@gmail.com>
2013-10-01 13:51:15 -0400

    add basic regex for zip and add flash errors

--- main.rb
+++ main.rb
@@ -4,18 +4,28 @@ require 'net/http'
 require 'sinatra'
 require 'sinatra/flash'
 require './env' if File.exists?('env.rb')
+enable :sessions
 
 get '/' do    
     erb :index  
 end  
 
 post '/' do
-	zip 			= params[:zip].to_s
-	key				= ENV['key']
-	url		 		= "http://api.petfinder.com/pet.find?key=#{key}&format=json&location=#{zip}"
-	resp   		= Net::HTTP.get_response(URI.parse(url))
-	resp_text = resp.body
-	obj 			= JSON.parse(resp_text)
-	@pets     = obj["petfinder"]["pets"]["pet"]
-	erb :index
+	zip 			= params[:zip]
+	if valid_zip zip
+		key				= ENV['key']
+		url		 		= "http://api.petfinder.com/pet.find?key=#{key}&format=json&location=#{zip}"
+		resp   		= Net::HTTP.get_response(URI.parse(url))
+		resp_text = resp.body
+		obj 			= JSON.parse(resp_text)
+		@pets     = obj["petfinder"]["pets"]["pet"]
+		erb :index
+	else
+		flash.now[:error] = "Please enter a 5 digit zip code."
+		erb :index
+	end
+end
+
+def valid_zip zip
+	zip.length == 5 && zip =~ /^\d+$/
 end
---
+++ spec/main_spec.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler'
+require 'rspec'
+require_relative '../main.rb'
+
+describe ""
